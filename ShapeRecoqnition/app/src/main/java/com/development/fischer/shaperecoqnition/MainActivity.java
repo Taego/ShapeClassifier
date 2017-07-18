a3dd250ba65f620c9050d223db6f50e279439d74
Fredrik Fischer <fredrik.fischer999@gmail.com>
2017-07-18 11:19:40 +0200

    Update
    Redid the application due to the errors induced when copying the application from the orgin
    
    The basic functionality is now working
    
    No vision functionality have been added

---
+++ ShapeRecoqnition/app/src/main/java/com/development/fischer/shaperecoqnition/MainActivity.java
@@ -0,0 +1,38 @@
+package com.development.fischer.shaperecoqnition;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void identify (View view){
+
+        Intent intent = new Intent(this, identify.class);
+        startActivity(intent);
+    }
+
+    public void settings (View view){
+
+        Intent intent = new Intent(this, settings.class);
+        startActivity(intent);
+    }
+
+    public void close (View view){
+
+        finish();
+    }
+
+}
